2cccc9a7d9e2d8416cfa5c010da40eef54edb50c
Thakur-ji-5513 <rudrakshthakur60@gmail.com>
2025-07-12 20:12:46 +0530

    practiced js

--- practice/js/1st.js
+++ practice/js/1st.js
@@ -1,4 +1,16 @@
-console.log("heyyaa");
-alert("hello");
-console.error("some error");
-console.warn("some warning");
+let num=[];
+let numb=prompt("Enter the number of elements you want to put");
+for(let i=0; i<numb; i++){
+    let inp=prompt("Enter the "+(i+1)+" number");
+    num[i]=inp;
+}
+let del=prompt("Enter the number which occurance you want to delete.");
+
+for(let i=0; i<numb; i++){
+    if(num[i]==del){
+        num.splice(i,1);
+    }
+}
+for(let number of num ){
+    console.log(number);
+}

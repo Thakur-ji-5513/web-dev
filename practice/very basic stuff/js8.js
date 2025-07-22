8c00bcaa78cf6828f90d15e9404b698e36cdfc47
Thakur-ji-5513 <rudrakshthakur60@gmail.com>
2025-07-22 09:41:29 +0530

    practiced js (9)

--- practice/very basic stuff/js8.js
+++ practice/very basic stuff/js8.js
@@ -1,6 +1,22 @@
-const arr=['h','e','l','l','o'];
+let but = document.createElement('button');
+let inp=document.createElement('input');
+let head=document.createElement('h1');
+let para=document.createElement("p");
 
-const newi = arr.map((el)=>{
-    return el.toUpperCase();
-})
-console.log(newi);
+
+inp.setAttribute('placeholder',"username");
+but.setAttribute('id',"btn");
+para.innerHTML="ApnaCollege <b>Delta</b> Practice."
+
+
+but.innerText=("click Me!!");
+document.body.prepend(but);
+document.body.prepend(inp);
+document.body.prepend(head);
+document.body.insertAdjacentElement("beforeend",para);
+
+document.getElementById('btn').style.backgroundColor="blue";
+document.querySelector('#btn').style.color="white";
+document.querySelector('h1').innerText="DOM Pracrise!";
+document.querySelector('h1').style.textDecoration="underline";
+document.querySelector('h1').style.color="purple";
